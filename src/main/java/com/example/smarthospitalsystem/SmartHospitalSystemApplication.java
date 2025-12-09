e09803648171fd82b9c195826c75de2920e91761
sha <s_sshadow@hotmail.com>
2025-12-09 18:26:18 +0300

    Initial commit: Backend and Frontend setup

---
+++ src/main/java/com/example/smarthospitalsystem/SmartHospitalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.smarthospitalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHospitalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHospitalSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smarthospitalsystem/SmartHospitalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smarthospitalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHospitalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
